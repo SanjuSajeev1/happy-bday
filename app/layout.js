5dcd577a4f7468dea0c998047b388e833d305ed3
SanjuSajeev1 <sanjusajeev055@gmail.com>
2025-08-21 22:40:04 +0530

    Improve code formatting and readability in CSS and layout files

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Indie+Flower&family=Kalam:wght@300;400;700&display=swap"
           rel="stylesheet"
